f53b46edf8c973944f8e5557c38153a9e85c607f
n8teiswashed <78764811+n8teiswashed@users.noreply.github.com>
2022-08-19 13:04:26 -0400

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,11 @@
+#twitter
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
+#spotifyt
+client_id = ''
+client_secret = ''
+redirect_uri = " http://localhost:8888/callback"
+scope = "user-modify-playback-state user-read-playback-state user-read-currently-playing user-follow-modify user-follow-read user-read-recently-played user-read-playback-position user-top-read playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private user-read-email user-read-private user-library-modify user-library-read"
---
+++ tweetit.py
@@ -0,0 +1,72 @@
+import requests, json, tweepy, spotipy, time
+from keys import *
+from spotipy.oauth2 import SpotifyOAuth
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+def auth(client_id, client_secret, redirect_uri, scope):
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
+                                                client_secret=client_secret,
+                                                redirect_uri=redirect_uri,
+                                                scope=scope))
+    
+def tweet_song(client_id, client_secret, redirect_uri, scope):
+    results = sp.current_user_playing_track()
+    artist_name = results['item']['artists'][0]['name']
+    song_name = results['item']['name']
+    song_link = results['item']['external_urls']['spotify']
+    album_name = results['item']['album']['name']
+    hashtag = f'#{artist_name.replace(" ", "")}'
+    tweet_string = f"I am now listening to 🔊\n🎶{song_name} - {artist_name}👨‍🎤\n💿Album - {album_name}💿\n{song_link}\n{hashtag}"
+    print('now tweeting...')
+    api.update_status(tweet_string)
+
+
+while True:
+    try:
+        auth(client_id, client_secret, redirect_uri, scope)
+        current_track = sp.current_user_playing_track()
+        current_track_id = current_track['item']['id']
+        time.sleep(5)
+
+        if current_track_id is not None:
+            tweet_song(client_id, client_secret, redirect_uri, scope)
+            break
+
+        else:
+            continue
+            time.sleep(5) 
+
+    except spotipy.oauth2.SpotifyOauthError as error:
+        auth(client_id, client_secret, redirect_uri, scope)
+        time.sleep(5)
+        print(f'spotify error {error}')
+
+    except BaseException as error:
+        print(error)
+        time.sleep(5)
+
+while True:
+    try:
+        auth(client_id, client_secret, redirect_uri, scope)
+        new_track = sp.current_user_playing_track()
+        new_track_id = new_track['item']['id']
+        time.sleep(5)
+
+        if current_track_id is not None and new_track_id != current_track_id:
+            tweet_song(client_id, client_secret, redirect_uri, scope)
+            current_track_id = new_track_id
+            time.sleep(5)
+        else:
+            continue
+            time.sleep(5)
+    except spotipy.oauth2.SpotifyOauthError as error:
+        auth(client_id, client_secret, redirect_uri, scope)
+        time.sleep(5)
+        print(f'spotify error {error}')
+    except BaseException as error:
+        print(error)   
+        time.sleep(5) 
